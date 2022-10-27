cf138f44f8875ab29adede744255d826a3221622
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-10-27 09:25:19 -0400

    Update 4.js

--- 4.js
+++ 4.js
@@ -3,10 +3,10 @@
 const numbers = [48, 20, 18, 7, 12,9, 8,10,12, 96, 3,36,51];
 
 let divid = numbers.filter(function(item)
-   {{if (item % 9 == 0)
-      return false
-    } { if (item % 3 == 0 && item % 6 == 0)
-      return true
+   {{if (item % 9 == 0) // cancelling out anything that can be divided by 9
+      return false //and making sure the array doesn't show any of those numbers
+    } { if (item % 3 == 0 && item % 6 == 0) //then, this will only take any numbers that can be divided by 3 and 6 from the if statement above
+      return true // and it'll return that true and it'll show only those numbers 
     }
 
 }
